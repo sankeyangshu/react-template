781fa044c01647c712b740b22dcaec5b943b32a2
sankeyangshu <1260323835@qq.com>
2023-06-29 14:37:18 +0800

    feat: 新增svg图标组件

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import 'virtual:svg-icons-register'; // svg-icons注册导入
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
