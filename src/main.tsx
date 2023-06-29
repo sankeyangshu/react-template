5152bbf1208965768aa56c65149dff3f3312b62d
sankeyangshu <1260323835@qq.com>
2023-06-29 14:41:00 +0800

    style: 新增全局样式

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import './styles/index.less'; // 全局css
 import 'virtual:svg-icons-register'; // svg-icons注册导入
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
