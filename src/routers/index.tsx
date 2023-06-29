a0dbf819dcec2dff544e36348270bf6fedcc1f2d
sankeyangshu <1260323835@qq.com>
2023-06-29 14:24:55 +0800

    feat: 新增路由模块

---
+++ src/routers/index.tsx
@@ -0,0 +1,21 @@
+import { useRoutes, RouteObject } from 'react-router-dom';
+import Home from '@/views/Home';
+
+/**
+ * 公共路由
+ */
+export const constantRoutes: RouteObject[] = [
+  {
+    path: '/',
+    id: 'Home',
+    element: <Home />,
+  },
+];
+
+// 创建一个可以被 React 应用程序使用的路由实例
+const router = () => {
+  const routes = useRoutes(constantRoutes);
+  return routes;
+};
+
+export default router;
---
+++ src/views/Home/index.tsx
@@ -0,0 +1,21 @@
+import reactLogo from '@/assets/react.svg';
+import viteLogo from '/vite.svg';
+import './index.less';
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+    </>
+  );
+};
+
+export default Home;
