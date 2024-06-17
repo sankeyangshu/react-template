5adb35c38cb213122555185fcaa186e9f13a7b3b
sankeyangshu <1260323835@qq.com>
2024-06-17 14:53:26 +0800

    style: lint

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './styles/index.less'; // 全局css
+import 'virtual:uno.css'; // 引入unocss
 import 'virtual:svg-icons-register'; // svg-icons注册导入
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
